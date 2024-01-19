55c730aa2e7c78f63f37f9fa5afb746279ae1770
mkkieru <mkieru55@gmail.com>
2024-01-19 12:11:30 +0300

    Push To save space

--- src/main/java/ke/co/skyworld/EndPoints/programs/GenerateProgramToken.java
+++ src/main/java/ke/co/skyworld/EndPoints/programs/GenerateProgramToken.java
@@ -55,7 +55,6 @@ public class GenerateProgramToken implements HttpHandler {
         // from 0 to 999999
         Random rnd = new Random();
         int number = rnd.nextInt(999999);
-
         // this will convert any number sequence into 6 character.
         return String.format("%06d", number);
     }
